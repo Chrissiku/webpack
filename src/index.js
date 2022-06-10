d6776d2dc9c1cdde2f7e50a4d033cc4ea46d922b
chrissiku <chrissiku5@gmail.com>
2022-06-10 20:46:17 +0200

    Linters installation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import generateJoke from "./generateJoke";
-import "./styles/style.css";
+import generateJoke from './generateJoke';
+import './styles/style.css';
 // Or you can use the syntax bellow to import your function
 // const generateJoke = require('./generateJoke');
 
 console.log(generateJoke());
-console.log("hello");
+console.log('hello');
